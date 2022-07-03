166c8879db2d67cfbb6260866cff66b3e4ce7bbe
dhanyastore <103267737+dhanyastore@users.noreply.github.com>
2022-07-03 16:06:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+        var first_name = "sai";
+        var middle_name = "dhanya";
+        var a = 10;
+        var b =5;
+        var add= a + b;
+        var full_name = first_name.concat(" ").concat(middle_name);
+        console.log("The full name is: " + full_name);
+        console.log(" a + b = " +add);
